5580f0a96e401541320145c6335661894eca551e
mrsu_company <marioasu@163.com>
2014-12-13 18:23:32 +0800

    20141213

--- dm_session.go
+++ dm_session.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
 	"sync"
 )
--- test.go
+++ test.go
@@ -3,16 +3,24 @@ package main
 import (
 	"fmt"
 	// "gopkg.in/mgo.v2/bson"
-	"math/rand"
+	"crypto/rand"
+	// "time"
 )
 
 func main() {
 	//fmt.Println(bson.M{"name": "mario"})
-	var m = make(map[string]int)
-	fmt.Println(m)
-	m["name"] = 25
-	if a, b := m["name"]; true {
-		fmt.Println(a)
-		fmt.Println(b)
-	}
+	// var m = make(map[string]int)
+	// fmt.Println(m)
+	// m["name"] = 25
+	// if a, b := m["name"]; true {
+	// 	fmt.Println(a)
+	// 	fmt.Println(b)
+	// }
+	// rand.Seed(time.Now().Unix())
+	// fmt.Println(rand.Int())
+	fmt.Println("------------")
+
+	fmt.Println(rand.Reader) // &{0 {0 0}}
+
+	fmt.Println("------------")
 }
