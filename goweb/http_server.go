1857c995ba84ed662a488db024b87916a2beb2e7
marioasu-sven-pc <marioasu@163.com>
2015-01-18 08:01:42 +0800

    add webserver

---
+++ goweb/http_server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"log"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() // 解析参数
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+
+	fmt.Fprintf(w, "hello marioasu") // 写入到输出
+}
+
+func main() {
+	http.HandleFunc("/", sayhelloName)
+	err := http.ListenAndServer(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServer: ", err)
+	}
+}
